66650771f4926f1d7a7517d9290a0ea77bf35cd8
Alex Chan <a.chan@wellcome.ac.uk>
2018-10-10 16:33:24 +0100

    Handle casting an array.array to a bytestring

--- gearman/compat.py
+++ gearman/compat.py
@@ -8,5 +8,11 @@ PY3 = sys.version_info[0] == 3
 
 if PY3:
     binary_type = bytes
+
+    def array_to_bytes(arr):
+        return arr.tobytes()
 else:
     binary_type = str
+
+    def array_to_bytes(arr):
+        return arr.tostring()
